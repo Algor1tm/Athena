d036c00f610fe0f23071d0d6cb54d7c69cf19d32
UserBug0 <lyiozel@gmail.com>
2022-07-24 15:40:51 +0300

    Fix Instrumentor

--- Athena/src/Athena/Debug/Instrumentor.h
+++ Athena/src/Athena/Debug/Instrumentor.h
@@ -197,28 +197,28 @@ namespace Athena
 	}
 }
 
-#define ATN_PROFILE 1
+#define ATN_PROFILE 0
 
 #if ATN_PROFILE
 // Resolve which function signature macro will be used. Note that this only
 // is resolved when the (pre)compiler starts, so the syntax highlighting
 // could mark the wrong one in your editor!
 	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
-	#define HZ_FUNC_SIG __PRETTY_FUNCTION__
+	#define ATN_FUNC_SIG __PRETTY_FUNCTION__
 	#elif defined(__DMC__) && (__DMC__ >= 0x810)
-	#define HZ_FUNC_SIG __PRETTY_FUNCTION__
+	#define ATN_FUNC_SIG __PRETTY_FUNCTION__
 	#elif (defined(__FUNCSIG__) || (_MSC_VER))
-	#define HZ_FUNC_SIG __FUNCSIG__
+	#define ATN_FUNC_SIG __FUNCSIG__
 	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-	#define HZ_FUNC_SIG __FUNCTION__
+	#define ATN_FUNC_SIG __FUNCTION__
 	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-	#define HZ_FUNC_SIG __FUNC__
+	#define ATN_FUNC_SIG __FUNC__
 	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-	#define HZ_FUNC_SIG __func__
+	#define ATN_FUNC_SIG __func__
 	#elif defined(__cplusplus) && (__cplusplus >= 201103)
-	#define HZ_FUNC_SIG __func__
+	#define ATN_FUNC_SIG __func__
 	#else
-	#define HZ_FUNC_SIG "HZ_FUNC_SIG unknown!"
+	#define ATN_FUNC_SIG "ATN_FUNC_SIG unknown!"
 	#endif
 
 	#define ATN_PROFILE_BEGIN_SESSION(name, filepath) ::Athena::Instrumentor::Get().BeginSession(name, filepath);
@@ -226,9 +226,9 @@ namespace Athena
 	#define ATN_PROFILE_SCOPE_LINE2(name, line) constexpr auto fixedName##line = ::Athena::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
 												   ::Athena::InstrumentationTimer timer##line(fixedName##line.Data)
 
-	#define ATN_PROFILE_SCOPE_LINE(name, line) HZ_PROFILE_SCOPE_LINE2(name, line)
-	#define ATN_PROFILE_SCOPE(name) ::Athena::InstrumentationTimer timer##__LINE__(name);
-	#define ATN_PROFILE_FUNCTION()	ATN_PROFILE_SCOPE(__FUNCSIG__);
+	#define ATN_PROFILE_SCOPE_LINE(name, line) ATN_PROFILE_SCOPE_LINE2(name, line)
+	#define ATN_PROFILE_SCOPE(name) ATN_PROFILE_SCOPE_LINE(name, __LINE__);
+	#define ATN_PROFILE_FUNCTION()	ATN_PROFILE_SCOPE(ATN_FUNC_SIG);
 
 #else
 	#define ATN_PROFILE_BEGIN_SESSION(name, filepath)
